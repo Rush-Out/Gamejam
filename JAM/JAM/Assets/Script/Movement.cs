01da793ce99445ae7d685e5acc678f2c2e5fd85a
Phantom_JP <joelphilipkon@gmail.com>
2020-08-02 21:13:51 +0530

    Tutorial Level
    1

---
+++ JAM/JAM/Assets/Script/Movement.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    private float moveInput;
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask typeOfGround;
+    public float jumpForce;
+    private float jumpTimeCounter;
+    public float jumptime;
+    private bool isJumping;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+
+    }
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, typeOfGround);
+
+
+
+        //for jumping and rotation
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+
+            isJumping = true;
+            jumpTimeCounter = jumptime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+}
